e9197805c7dee353e8b1bc626d84819d2783d897
Victor Georges <victorgeorges54@gmail.com>
2025-02-03 14:11:05 +0100

    ✨ Ajout du service d'authentification

--- back/app-auth/app/config/dependencies.php
+++ back/app-auth/app/config/dependencies.php
@@ -1,14 +1,13 @@
 <?php
-use toubeelib\core\services\auth\ServiceAuth;
+use geoquizz\core\services\auth\ServiceAuth;
 use Psr\Container\ContainerInterface;
-use toubeelib\core\services\auth\ServiceAuthInterface;
-use toubeelib\core\repositoryInterfaces\AuthRepositoryInterface;
-use toubeelib\infrastructure\PDO\PdoAuthRepository;
+use geoquizz\core\services\auth\ServiceAuthInterface;
+use geoquizz\core\repositoryInterfaces\AuthRepositoryInterface;
+use geoquizz\infrastructure\PDO\PdoAuthRepository;
 use app\providers\JWTManager;
 return [
     
     'auth.pdo' => function (ContainerInterface $container) {
-        // $config = parse_ini_file(__DIR__ . '/auth.db.ini');
         $configPath = $container->get('auth.db.config'); // Récupère le chemin défini dans settings.php
         $config = parse_ini_file($configPath);
         $dsn = "{$config['driver']}:host={$config['host']};dbname={$config['database']}";
--- back/app-auth/app/config/routes.php
+++ back/app-auth/app/config/routes.php
@@ -3,12 +3,22 @@
 use Slim\App;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use toubeelib\application\actions\HomeAction;
+use geoquizz\application\actions\HomeAction;
+use geoquizz\application\actions\RegisterAction;
+use geoquizz\application\actions\LoginAction;
+use geoquizz\application\actions\RefreshAction;
+use geoquizz\application\actions\ValidateTokenAction;
 
 return function(App $app): App {
 
     // Public routes
     $app->get('/', HomeAction::class)->setName('home');
+
+    //Authentification
+    $app->post('/auth/register', RegisterAction::class)->setName('register');
+    $app->post('/auth/login', LoginAction::class)->setName('login');
+    $app->post('/auth/refresh', RefreshAction::class)->setName('refresh');
+    $app->post('/tokens/validate', ValidateTokenAction::class);
                                                             
     $app->options('/{routes:.+}', function (Request $request, Response $response) {
         return $response;
